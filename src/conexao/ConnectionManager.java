0bf36fb9b195a5ff0dcdcbbcf4fab0709c16cbaa
Nestor <Nestor@Monteiro>
2014-10-29 01:41:57 -0200

    Connerction Manager implementada

--- src/conexao/ConnectionManager.java
+++ src/conexao/ConnectionManager.java
@@ -1,10 +1,63 @@
 package conexao;
 
-import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+
 
 public class ConnectionManager {
 
-	
+	private static ConnectionManager cm = null;
+
+	// Caminho para o driver
+	private static final String PATH_DRIVER = "com.mysql.jdbc.Driver";
+	// Usuario para o banco de dados
+	private static final String USR_BANCO = "root";
+	// Senha para o usuario do bando de dados
+	private static final String PASS_BANCO = "";
+	// Url de conexao para o banco de dados
+	private static final String CONN = "jdbc:mysql://localhost:3306/BancoCopa";
+
+	public static ConnectionManager getInstance() {
+		if (cm == null) {
+			cm = new ConnectionManager();
+		}
+		return cm;
+	}
+
+	ConnectionManager() {
+	}
+
+	public Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName(PATH_DRIVER);
+			connection = DriverManager.getConnection(CONN, USR_BANCO,
+					PASS_BANCO);
+			System.out.println("conctado a base");
+		} catch (ClassNotFoundException ex) {
+			Logger.getLogger(ConnectionManager.class.getName()).log(
+					Level.SEVERE, null, ex);
+		} catch (SQLException ex) {
+			Logger.getLogger(ConnectionManager.class.getName()).log(
+					Level.SEVERE, null, ex);
+		}
+		return connection;
+	}
+
+	public void closeConnection(Connection connection) {
+		try {
+			connection.close();
+		} catch (SQLException ex) {
+			Logger.getLogger(ConnectionManager.class.getName()).log(
+					Level.SEVERE, null, ex);
+		}
+	}	
 	
 	
 }

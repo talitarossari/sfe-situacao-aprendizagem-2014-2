68762ef04c9f41ac5e49e1a6933063c9d1635bb0
yuri_linconln <yuri_linconln@SNFNS2EP13VM111.florianopolis.sc.senai.br>
2014-11-13 21:55:35 -0200

    salaDaoImp

--- src/br/senai/sc/sfe/dao/SalaDao.java
+++ src/br/senai/sc/sfe/dao/SalaDao.java
@@ -1,7 +1,11 @@
 package br.senai.sc.sfe.dao;
 
+import java.util.List;
+
 import br.senai.sc.sfe.entity.Sala;
 
 public interface SalaDao extends dao<Sala> {
 
+	List<Sala> buscarPorId(int idSala);
+	
 }

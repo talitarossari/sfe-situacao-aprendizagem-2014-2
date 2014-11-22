fb75857270a74a0edd48021108c6ad1862296259
talitarossari <rossaritalita@hotmail.com>
2014-11-22 18:27:32 -0200

    UsuarioDao implementado

---
+++ src/br/senai/sc/sfe/dao/UsuarioDao.java
@@ -0,0 +1,63 @@
+package br.senai.sc.sfe.dao;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.senai.sc.sfe.entity.Sala;
+import br.senai.sc.sfe.entity.Usuario;
+import br.senai.sc.sfe.utils.JpaUtils;
+
+public class UsuarioDao {
+	
+	private EntityManager entityManager;
+
+	public UsuarioDao() {
+		JpaUtils.getInstance().createEntityManagerFactory();
+		entityManager = JpaUtils.getInstance().getEntityManager();
+	}
+
+	
+	public Usuario salvar(Usuario usuario){
+		try {
+			if (usuario.getIdUsuarios() == null) {
+				entityManager.persist(usuario);
+			} else {
+				entityManager.merge(usuario);
+			}
+		} catch (Exception e) {
+			Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+		}
+		return usuario;
+	}
+	
+	public void remover(int id){
+		Usuario usuario = entityManager.getReference(Usuario.class, id);
+		entityManager.remove(usuario);
+	}
+	
+	public List<Usuario> listar() {
+		Query query = entityManager.createQuery("From Usuario", Usuario.class);
+		return query.getResultList();
+	}
+	
+	public Usuario buscarPorId(int id) {
+		return entityManager.find(Usuario.class, id);
+	}
+	
+	public boolean buscarUsuario(String login, int senha){
+		Query query = entityManager.createQuery(
+				"From Usuario u where u.login = login and u.senha = senha", Usuario.class);
+		query.setParameter("login", login);
+		query.setParameter("senha", senha);
+		
+		if(query.getResultList() == null){
+			return false;			
+		} else {
+			return true;
+		}
+	}
+}
